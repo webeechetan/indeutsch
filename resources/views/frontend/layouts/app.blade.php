52307f272831fecfab6119a2ab84fd365b5c169a
webee-shahrukh <mohd.shahrukh@webeesocial.com>
2025-06-02 15:55:14 +0530

    Canvas Page Changes and Images Changes

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -40,6 +40,11 @@
 
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700;800&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+  <!-- Existing CSS files here -->
+@if(request()->routeIs('canvas')) 
+  <link rel="stylesheet" href="{{ asset('frontend/assets/css/custom.css') }}">
+@endif
+
 </head>
 
 <body
